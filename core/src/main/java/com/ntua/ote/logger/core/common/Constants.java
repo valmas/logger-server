cf3503a25f456687e84129709e8e0f89fd66507c
Nikolaos Valmas <Nikolaos.VALMAS@intrasoft-intl.com>
2017-03-17 18:27:02 +0200

    css

--- core/src/main/java/com/ntua/ote/logger/core/common/Constants.java
+++ core/src/main/java/com/ntua/ote/logger/core/common/Constants.java
@@ -4,11 +4,11 @@ public class Constants {
 
 	public static final String BLUE_MARKER_ICON = "http://maps.google.com/mapfiles/ms/micons/blue-dot.png";
 	
-	public static final String ENDPOINT_STYLE = "{fillStyle:'#47c9af'}";
+	public static final String ENDPOINT_STYLE = "{fillStyle:'#2399e5'}";
 	
 	public static final String ENDPOINT_SELECTED_STYLE = "{fillStyle:'#ff0000'}";
 	
-	public static final String CONNECTOR_PAINT_STYLE = "{strokeStyle: '#47c9af', lineWidth:3}";
+	public static final String CONNECTOR_PAINT_STYLE = "{strokeStyle: '#313b3f', lineWidth:3}";
 	
 	public static final String CONNECTOR_SELECTED_PAINT_STYLE = "{strokeStyle: '#ff0000', lineWidth:3}";
 	
